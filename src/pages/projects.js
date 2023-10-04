ccedc5db5b064758d8cf73a63630662b67873033
shahchhatru <shahkhesavchhatra@gmail.com>
2023-10-04 19:37:12 +0545

    portfolio tagline changed

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -50,8 +50,8 @@ export default function projects() {
       </Head>
       <main className="w-full mb-16 flex flex-col  items-center justify-center">
         <Layout className="pt-16">
-          <AnimatedText text=" Imagination Trumps knowledge" 
-            className="mb-16"
+          <AnimatedText text="Where imagination meets code, dreams take shape in the realm of reality."
+            className="mb-24"
           />
           <div className="grid  grid-cols-12 gap-24">
             <div className="col-span-12">
