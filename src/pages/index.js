edf66d8983a9312c52676cd1d0ebc34bc356af2b
shahchhatru <shahkhesavchhatra@gmail.com>
2023-10-04 19:22:29 +0545

    resume uploaded

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,14 +30,14 @@ Explore my latest projects and articles, showcasing my expertise in React.js and
 
         </p>
         <div className="flex items-center self-start mt-2">
-          <Link href="dummy.pdf" target={"_blank"} className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
+          <Link href="resume.pdf" target={"_blank"} className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
           border-2 border-solid border-transparent hover:border-dark"
           download={true}
           >
             Resume
             <LinkArrow className={'w-6 ml-1'}/>
             </Link>
-          <Link href="mailto:abc@gmail.com" target={"_blank"} className="ml-4 text-lg font-medium capitalize text-dark underline">Contact</Link>
+          <Link href="mailto:076bct021.chhatra@pcampus.edu.np" target={"_blank"} className="ml-4 text-lg font-medium capitalize text-dark underline">Contact</Link>
         </div>
       </div>
        </div>
